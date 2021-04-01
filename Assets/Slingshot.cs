99b67d50d7061589bfa0910f3d5bd34523b92767
John Sheehan <sheehanj@sunyulster.edu>
2021-04-01 12:05:56 -0400

    3-30-21 Class Version

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+	// fields set in the Unity Inspector
+	[Header("Set in Inspector")]
+	public GameObject prefabProjectile;
+	
+	// fields set dynamically
+	[Header("Set Dynamically")]
+	public GameObject launchPoint;
+	public Vector3  launchPos;
+	public GameObject  projectile;
+	public bool aimingMode;
+	
+	void Awake(){
+	   Transform launchPointTrans = transform.Find("LaunchPoint");
+       launchPoint = launchPointTrans.gameObject;
+	   launchPoint.SetActive(false);
+	   launchPos = launchPointTrans.position;
+	}
+	
+	void OnMouseDown(){
+	   aimingMode = true;
+
+       projectile = Instantiate(prefabProjectile) as GameObject;
+       projectile.transform.position = launchPos;
+       projectile.GetComponent<Rigidbody>().isKinematic = true;	   
+	}
+	
+	void OnMouseEnter(){
+	   //print("Slingshot:OnMouseEnter()");
+       launchPoint.SetActive(true);	   
+	}
+	
+	void OnMouseExit(){
+	   //print("Slingshot:OnMouseExit()");
+	   launchPoint.SetActive(false);
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
